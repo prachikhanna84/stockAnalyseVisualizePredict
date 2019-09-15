589c7963839134489457b968a2c84e5a351991aa
prachikhanna <er.prachikhanna@gmail.com>
2019-09-15 16:03:51 -0500

    ui

---
+++ stockdataUI/plots.js
@@ -0,0 +1,88 @@
+var apiKey = "KQbxmBerCsQ2H7JZ_YB8";
+
+/**
+ * Helper function to select stock data
+ * Returns an array of values
+ * @param {array} rows
+ * @param {integer} index
+ * index 0 - Date
+ * index 1 - Open
+ * index 2 - High
+ * index 3 - Low
+ * index 4 - Close
+ * index 5 - Volume
+ */
+function unpack(rows, index) {
+  return rows.map(function(row) {
+    return row[index];
+  });
+}
+
+function buildPlot() {
+  var url = `http://localhost:5000/api/v1.0/getData/AAPL`;
+
+  d3.json(url).then(function(data) {
+
+    console.log(data);
+    // Grab values from the response json object to build the plots
+    var name = data.dataset.name;
+    var stock = data.dataset.dataset_code;
+    var startDate = data.dataset.start_date;
+    var endDate = data.dataset.end_date;
+    var dates = unpack(data.dataset.data, 0);
+    var openingPrices = unpack(data.dataset.data, 1);
+    var highPrices = unpack(data.dataset.data, 2);
+    var lowPrices = unpack(data.dataset.data, 3);
+    var closingPrices = unpack(data.dataset.data, 4);
+
+    var trace1 = {
+      type: "scatter",
+      mode: "lines",
+      name: name,
+      x: dates,
+      y: closingPrices,
+      line: {
+        color: "#17BECF"
+      }
+    };
+
+    // Candlestick Trace
+    var trace2 = {
+      type: "candlestick",
+      x: dates,
+      high: highPrices,
+      low: lowPrices,
+      open: openingPrices,
+      close: closingPrices
+    };
+
+    var data = [trace1, trace2];
+
+    var layout = {
+      title: `${stock} closing prices`,
+      xaxis: {
+        range: [startDate, endDate],
+        type: "date"
+      },
+      yaxis: {
+        autorange: true,
+        type: "linear"
+      },
+      showlegend: false
+    };
+
+    Plotly.newPlot("plot", data, layout);
+
+  });
+}
+
+buildPlot();
+
+// BONUS - Dynamically add the current date to the report header
+var monthNames = [
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var today = new Date();
+var date = `${monthNames[today.getMonth()]} ${today.getFullYear().toString().substr(2, 2)}`;
+
+// d3.select("#report-date").text(date);
